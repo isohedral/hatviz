f8a21ae3e5580c7fbfac22900f35c0a3b7d6028a
Craig S. Kaplan <azriel@gmail.com>
2023-04-22 20:11:55 -0400

    Lots of refactoring and re-architecting
    Moved a lot of code around and built new abstractions to make everything simpler and easier to read.  Some parts were inspired by ideas from @johanvts.

---
+++ geometry.js
@@ -0,0 +1,96 @@
+const r3 = 1.7320508075688772;
+const hr3 = 0.8660254037844386;
+const ident = [1, 0, 0, 0, 1, 0];
+
+function pt( x, y )
+{
+	return { x : x, y : y };
+}
+
+function hexPt( x, y )
+{
+	return pt( x + 0.5*y, hr3*y );
+}
+
+// Affine matrix inverse
+function inv( T ) {
+	const det = T[0]*T[4] - T[1]*T[3];
+	return [T[4]/det, -T[1]/det, (T[1]*T[5]-T[2]*T[4])/det,
+		-T[3]/det, T[0]/det, (T[2]*T[3]-T[0]*T[5])/det];
+};
+
+// Affine matrix multiply
+function mul( A, B )
+{
+	return [A[0]*B[0] + A[1]*B[3], 
+		A[0]*B[1] + A[1]*B[4],
+		A[0]*B[2] + A[1]*B[5] + A[2],
+
+		A[3]*B[0] + A[4]*B[3], 
+		A[3]*B[1] + A[4]*B[4],
+		A[3]*B[2] + A[4]*B[5] + A[5]];
+}
+
+function padd( p, q )
+{
+	return { x : p.x + q.x, y : p.y + q.y };
+}
+
+function psub( p, q )
+{
+	return { x : p.x - q.x, y : p.y - q.y };
+}
+
+// Rotation matrix
+function trot( ang )
+{
+	const c = cos( ang );
+	const s = sin( ang );
+	return [c, -s, 0, s, c, 0];
+}
+
+// Translation matrix
+function ttrans( tx, ty )
+{
+	return [1, 0, tx, 0, 1, ty];
+}
+
+function rotAbout( p, ang )
+{
+	return mul( ttrans( p.x, p.y ), 
+		mul( trot( ang ), ttrans( -p.x, -p.y ) ) );
+}
+
+// Matrix * point
+function transPt( M, P )
+{
+	return pt(M[0]*P.x + M[1]*P.y + M[2], M[3]*P.x + M[4]*P.y + M[5]);
+}
+
+// Match unit interval to line segment p->q
+function matchSeg( p, q )
+{
+	return [q.x-p.x, p.y-q.y, p.x,  q.y-p.y, q.x-p.x, p.y];
+};
+
+// Match line segment p1->q1 to line segment p2->q2
+function matchTwo( p1, q1, p2, q2 )
+{
+	return mul( matchSeg( p2, q2 ), inv( matchSeg( p1, q1 ) ) );
+};
+
+// Intersect two lines defined by segments p1->q1 and p2->q2
+function intersect( p1, q1, p2, q2 )
+{
+    const d = (q2.y - p2.y) * (q1.x - p1.x) - (q2.x - p2.x) * (q1.y - p1.y);
+    const uA = ((q2.x - p2.x) * (p1.y - p2.y) - (q2.y - p2.y) * (p1.x - p2.x)) / d;
+    const uB = ((q1.x - p1.x) * (p1.y - p2.y) - (q1.y - p1.y) * (p1.x - p2.x)) / d;
+
+    return pt( p1.x + uA * (q1.x - p1.x), p1.y + uA * (q1.y - p1.y) );
+}
+
+const hat_outline = [
+    hexPt(0, 0), hexPt(-1,-1), hexPt(0,-2), hexPt(2,-2),
+    hexPt(2,-1), hexPt(4,-2), hexPt(5,-1), hexPt(4, 0),
+    hexPt(3, 0), hexPt(2, 2), hexPt(0, 3), hexPt(0, 2),
+    hexPt(-1, 2) ];
--- hat.js
+++ hat.js
@@ -1,7 +1,3 @@
-const r3 = 1.7320508075688772;
-const hr3 = 0.8660254037844386;
-const ident = [1,0,0,0,1,0];
-
 let to_screen = [20, 0, 0, 0, -20, 0];
 let lw_scale = 1;
 let tiles;
@@ -18,148 +14,32 @@ let scale_button;
 let draw_hats;
 let draw_super;
 let radio;
-let cp_h1;
-let cp_h;
-let cp_t;
-let cp_p;
-let cp_f;
+
 let dragging = false;
 let uibox = true;
 let box_height = 10;
 
-let H1_hat;
-let H_hat;
-let T_hat;
-let P_hat;
-let F_hat;
-
 let svg_serial = 0;
-let h1_color = [0, 137, 212];
-let h_color = [148, 205, 235];
-let t_color = [251, 251, 251];
-let p_color = [250, 250, 250];
-let f_color = [191, 191, 191];
 
-function color2rgb(c)
-{
-	return [c.levels[0], c.levels[1], c.levels[2]];
-}
-function set_h1_color()
-{
-	H1_hat.fill = color2rgb(cp_h1.color());
-}
-function set_h_color()
-{
-	H_hat.fill = color2rgb(cp_h.color());
-}
-function set_t_color()
-{
-	T_hat.fill = color2rgb(cp_t.color());
-}
-function set_p_color()
-{
-	P_hat.fill = color2rgb(cp_p.color());
-}
-function set_f_color()
-{
-	F_hat.fill = color2rgb(cp_f.color());
-}
+const cols = {};
+let black;
 
-function pt( x, y )
+function getSVGID()
 {
-	return { x : x, y : y };
-}
-
-function hexPt( x, y )
-{
-	return pt( x + 0.5*y, hr3*y );
-}
-
-// Affine matrix inverse
-function inv( T ) {
-	const det = T[0]*T[4] - T[1]*T[3];
-	return [T[4]/det, -T[1]/det, (T[1]*T[5]-T[2]*T[4])/det,
-		-T[3]/det, T[0]/det, (T[2]*T[3]-T[0]*T[5])/det];
-};
-
-// Affine matrix multiply
-function mul( A, B )
-{
-	return [A[0]*B[0] + A[1]*B[3], 
-		A[0]*B[1] + A[1]*B[4],
-		A[0]*B[2] + A[1]*B[5] + A[2],
-
-		A[3]*B[0] + A[4]*B[3], 
-		A[3]*B[1] + A[4]*B[4],
-		A[3]*B[2] + A[4]*B[5] + A[5]];
-}
-
-function padd( p, q )
-{
-	return { x : p.x + q.x, y : p.y + q.y };
-}
-
-function psub( p, q )
-{
-	return { x : p.x - q.x, y : p.y - q.y };
-}
-
-// Rotation matrix
-function trot( ang )
-{
-	const c = cos( ang );
-	const s = sin( ang );
-	return [c, -s, 0, s, c, 0];
-}
-
-// Translation matrix
-function ttrans( tx, ty )
-{
-	return [1, 0, tx, 0, 1, ty];
-}
-
-function rotAbout( p, ang )
-{
-	return mul( ttrans( p.x, p.y ), 
-		mul( trot( ang ), ttrans( -p.x, -p.y ) ) );
-}
-
-// Matrix * point
-function transPt( M, P )
-{
-	return pt(M[0]*P.x + M[1]*P.y + M[2], M[3]*P.x + M[4]*P.y + M[5]);
-}
-
-// Match unit interval to line segment p->q
-function matchSeg( p, q )
-{
-	return [q.x-p.x, p.y-q.y, p.x,  q.y-p.y, q.x-p.x, p.y];
-};
-
-// Match line segment p1->q1 to line segment p2->q2
-function matchTwo( p1, q1, p2, q2 )
-{
-	return mul( matchSeg( p2, q2 ), inv( matchSeg( p1, q1 ) ) );
-};
-
-function intersect( p1, q1, p2, q2 )
-{
-    const d = (q2.y - p2.y) * (q1.x - p1.x) - (q2.x - p2.x) * (q1.y - p1.y);
-    const uA = ((q2.x - p2.x) * (p1.y - p2.y) - (q2.y - p2.y) * (p1.x - p2.x)) / d;
-    const uB = ((q1.x - p1.x) * (p1.y - p2.y) - (q1.y - p1.y) * (p1.x - p2.x)) / d;
-
-    return pt( p1.x + uA * (q1.x - p1.x), p1.y + uA * (q1.y - p1.y) );
+	const ret = 't' + String(svg_serial).padStart( 5, '0' );
+	++svg_serial;
+	return ret;
 }
 
 function drawPolygon( shape, T, f, s, w )
 {
 	if( f != null ) {
-		fill( ...f );
+		fill( f );
 	} else {
 		noFill();
 	}
 	if( s != null ) {
-		stroke( ...s );
+		stroke( s );
 		strokeWeight( w * lw_scale );
 	} else {
 		noStroke();
@@ -172,14 +52,97 @@ function drawPolygon( shape, T, f, s, w )
 	endShape( CLOSE );
 }
 
-class Geom
+function polygonToSVG( shape, id, f, s, w )
 {
-	constructor( pgon, fill, stroke ) 
+	let verts = '';
+	for( let p of shape ) {
+		if( verts.length > 0 ) {
+			verts = verts + ' ';
+		}
+		verts = verts + p.x + ',' + p.y;
+	}
+
+	let ids = '';
+	if( id != null ) {
+		ids = ` id="${id}"`;
+	}
+
+	let str = ' stroke="none"';
+	if( s != null ) {
+		str = ` stroke="rgb(${red(s)},${green(s)},${blue(s)})" stroke-width="${w}"`;
+	}
+
+	let fil = ' fill="none"';
+	if( f != null ) {
+		fil = ` fill="rgb(${red(f)},${green(f)},${blue(f)})"`;
+	}
+
+	return `    <polygon${ids} points="${verts}"${str}${fil}/>`;
+}
+
+function getSVGInstance( id, T )
+{
+	return `    <use xlink:href="#${id}" transform="matrix(${T[0]} ${T[3]} ${T[1]} ${T[4]} ${T[2]} ${T[5]})"/>`;
+}
+
+// The base level of the scene, a single hat tile, including a label
+// for colouring
+class HatTile
+{
+	constructor( label )
 	{
-		this.shape = pgon;
-		this.fill = fill
-		this.stroke = stroke;
-		this.width = 1.0;
+		this.label = label;
+		this.svg_id = null;
+	}
+
+	draw( S, level )
+	{
+		drawPolygon( 
+			hat_outline, S, cols[this.label].color(), black, 1 );
+	}
+
+	resetSVG()
+	{
+		this.svg_id = null;
+	}
+
+	buildSVGDefs( stream, sc )
+	{
+		if( this.svg_id != null ) {
+			return;
+		}
+
+		this.svg_id = getSVGID();
+		stream.push( polygonToSVG( hat_outline, this.svg_id, 
+			cols[this.label].color(), black, lw_scale/sc ) );
+	}
+
+	getSVGStrokeID()
+	{
+		return null;
+	}
+
+	getSVGFillID()
+	{
+		return this.svg_id;
+	}
+
+	getText( stream, T )
+	{
+		// Write out the top two rows of an affine transformation matrix
+		// giving the location of this hat, together with the type of 
+		// this tile.
+		stream.push( `${this.label} ${T[0]} ${T[1]} ${T[2]} ${T[3]} ${T[4]} ${T[5]}` )
+	}
+}
+
+// A group that collects a list of transformed children and an outline
+class MetaTile
+{
+	constructor( shape, width )
+	{
+		this.shape = shape;
+		this.width = width;
 		this.children = [];
 		this.svg_id = null;
 	}
@@ -201,7 +164,7 @@ class Geom
 				g.geom.draw( mul( S, g.T ), level - 1 );
 			}
 		} else {
-			drawPolygon( this.shape, S, this.fill, this.stroke, this.width );
+			drawPolygon( this.shape, S, null, black, this.width );
 		}
 	}
 
@@ -241,172 +204,135 @@ class Geom
 			return;
 		}
 
-		this.svg_id = 't' + String(svg_serial).padStart( 5, '0' );
-		++svg_serial;
+		this.svg_id = getSVGID();
 
 		for( let ch of this.children ) {
-			const T = ch.T;
-			ch.geom.buildSVGDefs( stream, sc * mag( T[0], T[1] ) );
+			ch.geom.buildSVGDefs( stream, sc );
 		}
 
-		// Stroked group
-		stream.push( `  <g id="${this.svg_id}s">` );
+		// Construct a fill group that must live at a logical lowest
+		// layer in the draw order.
 
+		stream.push( `  <g id="${this.getSVGFillID()}">` );
 		for( let ch of this.children ) {
-			const T = ch.T;
-			const gid = ch.geom.svg_id;
-			stream.push( `    <use xlink:href="#${gid}s" transform="matrix(${T[0]} ${T[3]} ${T[1]} ${T[4]} ${T[2]} ${T[5]})"/>` );
-		}
-
-		if( (this.shape.length > 0) && (this.fill == null) ) {
-			let verts = '';
-			for( let p of this.shape ) {
-				if( verts.length > 0 ) {
-					verts = verts + ' ';
-				}
-				verts = verts + p.x + ',' + p.y;
+			const fid = ch.geom.getSVGFillID();
+			if( fid != null ) {
+				stream.push( getSVGInstance( fid, ch.T ) );
 			}
-			let str = ' stroke="none"';
-			if( this.stroke != null ) {
-				let st = this.stroke;
-				if( st.length == 1 ) {
-					st = [st[0], st[0], st[0]];
-				}
-
-				 str = ` stroke="rgb(${st[0]},${st[1]},${st[2]})" stroke-width="${this.width*lw_scale/sc}"`;
-			}
-
-			stream.push( `    <polygon points="${verts}" fill="none"${str}/>` );
 		}
-
 		stream.push( '  </g>' );
 
-		// Filled group
-		stream.push( `  <g id="${this.svg_id}f">` );
+		// Construct a stroke group that must live above all fill groups.
 
+		stream.push( `  <g id="${this.getSVGStrokeID()}">` );
 		for( let ch of this.children ) {
-			const T = ch.T;
-			const gid = ch.geom.svg_id;
-			stream.push( `    <use xlink:href="#${gid}f" transform="matrix(${T[0]} ${T[3]} ${T[1]} ${T[4]} ${T[2]} ${T[5]})"/>` );
+			const sid = ch.geom.getSVGStrokeID();
+			if( sid != null ) {
+				stream.push( getSVGInstance( sid, ch.T ) );
+			}
 		}
-
-		if( (this.shape.length > 0) && (this.fill != null) ) {
-			let verts = '';
-			for( let p of this.shape ) {
-				if( verts.length > 0 ) {
-					verts = verts + ' ';
-				}
-				verts = verts + p.x + ',' + p.y;
-			}
-			let str = ' stroke="none"';
-			if( this.stroke != null ) {
-				let st = this.stroke;
-				if( st.length == 1 ) {
-					st = [st[0], st[0], st[0]];
-				}
-
-				 str = ` stroke="rgb(${st[0]},${st[1]},${st[2]})" stroke-width="${this.width*lw_scale/sc}"`;
-			}
-
-			let fil = ' fill="none"';
-			if( this.fill != null ) {
-				fil = ` fill="rgb(${this.fill[0]},${this.fill[1]},${this.fill[2]})"`;
-			}
-
-			stream.push( `    <polygon points="${verts}"${str}${fil}/>` );
-		}
+		stream.push( polygonToSVG( this.shape, 
+			null, null, black, this.width*lw_scale/sc ) );
 
 		stream.push( '  </g>' );
 	}
+
+	getSVGStrokeID()
+	{
+		return `${this.svg_id}s`;
+	}
+
+	getSVGFillID()
+	{
+		return `${this.svg_id}f`;
+	}
+
+	getText( stream, T )
+	{
+		for( let g of this.children ) {
+			g.geom.getText( stream, mul( T, g.T ) );
+		}
+	}
 }
 
-const hat_outline = [
-    hexPt(0, 0), hexPt(-1,-1), hexPt(0,-2), hexPt(2,-2),
-    hexPt(2,-1), hexPt(4,-2), hexPt(5,-1), hexPt(4, 0),
-    hexPt(3, 0), hexPt(2, 2), hexPt(0, 3), hexPt(0, 2),
-    hexPt(-1, 2) ];
-
-H1_hat = new Geom( hat_outline, h1_color, [0, 0, 0] );
-H_hat = new Geom( hat_outline, h_color, [0, 0, 0] );
-T_hat = new Geom( hat_outline, t_color, [0, 0, 0] );
-P_hat = new Geom( hat_outline, p_color, [0, 0, 0] );
-F_hat = new Geom( hat_outline, f_color, [0, 0, 0] );
+const H1_hat = new HatTile( 'H1' );
+const H_hat = new HatTile( 'H' );
+const T_hat = new HatTile( 'T' );
+const P_hat = new HatTile( 'P' );
+const F_hat = new HatTile( 'F' );
 
 const H_init = (function () {
 	const H_outline = [
 		pt( 0, 0 ), pt( 4, 0 ), pt( 4.5, hr3 ),
 		pt( 2.5, 5 * hr3 ), pt( 1.5, 5 * hr3 ), pt( -0.5, hr3 ) ];
-	geom = new Geom( H_outline, null, [0,0,0] );
-	geom.width = 2;
+	const meta = new MetaTile( H_outline, 2 );
 
-	geom.addChild( 
+	meta.addChild( 
 		matchTwo( 
 			hat_outline[5], hat_outline[7], H_outline[5], H_outline[0] ),
 		H_hat );
-	geom.addChild( 
+	meta.addChild( 
 		matchTwo( 
 			hat_outline[9], hat_outline[11], H_outline[1], H_outline[2] ),
 		H_hat );
-	geom.addChild( 
+	meta.addChild( 
 		matchTwo( 
 			hat_outline[5], hat_outline[7], H_outline[3], H_outline[4] ),
 		H_hat );
-	geom.addChild( 
+	meta.addChild( 
 		mul( ttrans( 2.5, hr3 ), 
 			mul( 
 				[-0.5,-hr3,0,hr3,-0.5,0],
 				[0.5,0,0,0,-0.5,0] ) ),
 		H1_hat );
 
-	return geom; }());
+	return meta; }());
 
 const T_init = (function () {
 	const T_outline = [
 		pt( 0, 0 ), pt( 3, 0 ), pt( 1.5, 3 * hr3 ) ];
-	geom = new Geom( T_outline, null, [0,0,0] );
-	geom.width = 2;
+	const meta = new MetaTile( T_outline, 2 );
 
-	geom.addChild( 
+	meta.addChild( 
 		[0.5, 0, 0.5, 0, 0.5, hr3],
 		T_hat );
 
-	return geom; }());
+	return meta; }());
 
 const P_init = (function () {
 	const P_outline = [
 		pt( 0, 0 ), pt( 4, 0 ), 
 		pt( 3, 2 * hr3 ), pt( -1, 2 * hr3 ) ];
-	geom = new Geom( P_outline, null, [0,0,0] );
-	geom.width = 2;
+	const meta = new MetaTile( P_outline, 2 );
 
-	geom.addChild( 
+	meta.addChild( 
 		[0.5, 0, 1.5, 0, 0.5, hr3],
 		P_hat );
-	geom.addChild( 
+	meta.addChild( 
 		mul( ttrans( 0, 2 * hr3 ), 
 			mul( [0.5, hr3, 0, -hr3, 0.5, 0],
 				 [0.5, 0.0, 0.0, 0.0, 0.5, 0.0] ) ),
 		P_hat );
 
-	return geom; }());
+	return meta; }());
 
 const F_init = (function () {
 	const F_outline = [
 		pt( 0, 0 ), pt( 3, 0 ), 
 		pt( 3.5, hr3 ), pt( 3, 2 * hr3 ), pt( -1, 2 * hr3 ) ];
-	geom = new Geom( F_outline, null, [0,0,0] );
-	geom.width = 2;
+	const meta = new MetaTile( F_outline, null, [0,0,0] );
+	meta.width = 2;
 
-	geom.addChild( 
+	meta.addChild( 
 		[0.5, 0, 1.5, 0, 0.5, hr3],
 		F_hat );
-	geom.addChild( 
+	meta.addChild( 
 		mul( ttrans( 0, 2 * hr3 ), 
 			mul( [0.5, hr3, 0, -hr3, 0.5, 0],
 				 [0.5, 0.0, 0.0, 0.0, 0.5, 0.0] ) ),
 		F_hat );
 
-	return geom; }());
+	return meta; }());
 
 function constructPatch( H, T, P, F )
 {
@@ -442,8 +368,7 @@ function constructPatch( H, T, P, F )
 		[4, 0, 'F', 3] 
 		];
 
-	ret = new Geom( [], null, null );
-	ret.width = H.width;
+	ret = new MetaTile( [], H.width );
 	shapes = { 'H' : H, 'T' : T, 'P' : P, 'F' : F };
 
 	for( let r of rules ) {
@@ -499,15 +424,13 @@ function constructMetatiles( patch )
 	new_H_outline.push( psub( new_H_outline[3], w ) );
 	new_H_outline.push( patch.evalChild( 6, 2 ) );
 
-	const new_H = new Geom( new_H_outline, null, [0,0,0] );
-	new_H.width = patch.width * 2;
+	const new_H = new MetaTile( new_H_outline, patch.width * 2 );
 	for( let ch of [0, 9, 16, 27, 26, 6, 1, 8, 10, 15] ) {
 		new_H.addChild( patch.children[ch].T, patch.children[ch].geom );
 	}
 
 	const new_P_outline = [ p72, padd( p72, psub( bps1, llc ) ), bps1, llc ];
-	const new_P = new Geom( new_P_outline, null, [0,0,0] );
-	new_P.width = patch.width * 2;
+	const new_P = new MetaTile( new_P_outline, patch.width * 2 );
 	for( let ch of [7,2,3,4,28] ) {
 		new_P.addChild( patch.children[ch].T, patch.children[ch].geom );
 	}
@@ -515,8 +438,7 @@ function constructMetatiles( patch )
 	const new_F_outline = [ 
 		bps2, patch.evalChild( 24, 2 ), patch.evalChild( 25, 0 ),
 		p252, padd( p252, psub( llc, bps1 ) ) ];
-	const new_F = new Geom( new_F_outline, null, [0,0,0] );
-	new_F.width = patch.width * 2;
+	const new_F = new MetaTile( new_F_outline, patch.width * 2 );
 	for( let ch of [21,20,22,23,24,25] ) {
 		new_F.addChild( patch.children[ch].T, patch.children[ch].geom );
 	}
@@ -526,8 +448,7 @@ function constructMetatiles( patch )
 		psub( new_H_outline[4], new_H_outline[5] ) );
 	const CCC = transPt( rotAbout( BBB, -PI/3 ), AAA );
 	const new_T_outline = [BBB,CCC,AAA];
-	const new_T = new Geom( new_T_outline, null, [0,0,0] );
-	new_T.width = patch.width * 2;
+	const new_T = new MetaTile( new_T_outline, patch.width * 2 );
 	new_T.addChild( patch.children[11].T, patch.children[11].geom );
 
 	new_H.recentre();
@@ -548,16 +469,26 @@ function setButtonActive( but, b )
 	but.elt.style.border = (b ? "3px solid black" : "");
 }
 
+function addButton( name, f )
+{
+	const ret = createButton( name );
+	ret.position( 10, box_height );
+	ret.size( 125, 25 );
+	ret.mousePressed( f );
+	box_height += 30;
+
+	return ret;
+}
+
 function setup() {
 	createCanvas( windowWidth, windowHeight );
 
 	tiles = [H_init, T_init, P_init, F_init];
 	level = 1;
 
-	reset_button = createButton( "Reset" );
-	reset_button.position( 10, box_height );
-	reset_button.size( 125, 25 );
-	reset_button.mousePressed( function() {
+	black = color( 'black' );
+
+	reset_button = addButton( "Reset", function() {
 		tiles = [H_init, T_init, P_init, F_init];
 		level = 1;
 		radio.selected( 'H' );
@@ -567,18 +498,13 @@ function setup() {
 		setButtonActive( draw_super, true );
 		loop();
 	} );
-	box_height += 30;
-
-	subst_button = createButton( "Build Supertiles" );
-	subst_button.position( 10, box_height );
-	subst_button.size( 125, 25 );
-	subst_button.mousePressed( function() {
+	subst_button = addButton( "Build Supertiles", function() {
 		const patch = constructPatch( ...tiles );
 		tiles = constructMetatiles( patch );
 		++level;
 		loop();
 	} );
-	box_height += 40;
+	box_height += 10;
 
 	radio = createRadio();
 	radio.mousePressed( function() { loop() } );
@@ -590,93 +516,70 @@ function setup() {
 	radio.selected( 'H' );
 	box_height += 40;
 
-	cp_h1 = createColorPicker( color( ...h1_color ) );
-	cp_h1.mousePressed( function() { loop() } );
-	cp_h1.input(set_h1_color);
-	cp_h1.position( 10, box_height );
-	box_height += 40;
+	const cp_info = {
+		'H1' : [0, 137, 212],
+		'H' : [148, 205, 235],
+		'T' : [251, 251, 251],
+		'P' : [250, 250, 250],
+		'F' : [191, 191, 191]
+	};
 
-	cp_h = createColorPicker( color( ...h_color) );
-	cp_h.mousePressed( function() { loop() } );
-	cp_h.input(set_h_color);
-	cp_h.position( 10, box_height );
-	box_height += 40;
+	let count = 0;
+	for( let [name, col] of Object.entries( cp_info ) ) {
+		const label = createSpan( name );
+		label.position( 10 + 70*count, box_height );
+		const cp = createColorPicker( color( ...col ) );
+		cp.mousePressed( function() { loop() } );
+		cp.position( 10 + 70*count, box_height + 20 );
+		cols[name] = cp;
 
-	cp_t = createColorPicker( color( ...t_color ) );
-	cp_t.mousePressed( function() { loop() } );
-	cp_t.input(set_t_color);
-	cp_t.position( 10, box_height );
-	box_height += 40;
+		++count;
+		if( count == 2 ) {
+			count = 0;
+			box_height += 50;
+		}
+	}
+	if( count == 1 ) {
+		box_height += 50;
+	}
+	box_height += 20;
 
-	cp_p = createColorPicker( color( ...p_color) );
-	cp_p.mousePressed( function() { loop() } );
-	cp_p.input(set_p_color);
-	cp_p.position( 10, box_height );
-	box_height += 40;
-
-	cp_f = createColorPicker( color( ...f_color ) );
-	cp_f.mousePressed( function() { loop() } );
-	cp_f.input(set_f_color);
-	cp_f.position( 10, box_height );
-	box_height += 40;
-
-	translate_button = createButton( "Translate" );
-	setButtonActive( translate_button, true );
-	translate_button.position( 10, box_height );
-	translate_button.size( 125, 25 );
-	translate_button.mousePressed( function() {
+	translate_button = addButton( "Translate", function() {
 		setButtonActive( translate_button, true );
 		setButtonActive( scale_button, false );
 		loop();
 	} );
-	box_height += 30;
-
-	scale_button = createButton( "Scale" );
-	scale_button.position( 10, box_height );
-	scale_button.size( 125, 25 );
-	scale_button.mousePressed( function() {
+	scale_button = addButton( "Scale", function() {
 		setButtonActive( translate_button, false );
 		setButtonActive( scale_button, true );
 		loop();
 	} );
-	box_height += 30;
+
+	setButtonActive( translate_button, true );
+	box_height += 10;
 	
-	draw_hats = createButton( "Draw Hats" );
-	setButtonActive( draw_hats, true );
-	draw_hats.mousePressed( function() {
+	draw_hats = addButton( "Draw Hats", function() {
 		setButtonActive( draw_hats, !isButtonActive( draw_hats ) );
 		loop();
 	} );
-	draw_hats.position( 10, box_height );
-	draw_hats.size( 125, 25 );
-	box_height += 30;
-
-	draw_super = createButton( "Draw Supertiles" );
-	setButtonActive( draw_super, true );
-	draw_super.mousePressed( function() {
+	draw_super = addButton( "Draw Supertiles", function() {
 		setButtonActive( draw_super, !isButtonActive( draw_super ) );
 		loop();
 	} );
-	draw_super.position( 10, box_height );
-	draw_super.size( 125, 25 );
-	box_height += 40;
 
-	let save_button = createButton( "Save PNG" );
-	save_button.position( 10, box_height );
-	save_button.size( 125, 25 );
-	save_button.mousePressed( function () {
+	setButtonActive( draw_hats, true );
+	setButtonActive( draw_super, true );
+	box_height += 10;
+
+	addButton( "Save PNG", function () {
 		uibox = false;
 		draw();
 		save( "output.png" );
 		uibox = true;
 		draw();
 	} );
-	box_height += 30;
 
-	let svg_button = createButton( "Save SVG" );
-	svg_button.position( 10, box_height );
-	svg_button.size( 125, 25 );
-	svg_button.mousePressed( function () {
+	addButton( "Save SVG", function () {
 		svg_serial = 0;
 		for( let t of tiles ) {
 			t.resetSVG();
@@ -691,18 +594,26 @@ function setup() {
 		stream.push( '</defs>' );
 
 		const idx = {'H':0, 'T':1, 'P':2, 'F':3}[radio.value()];
+		const S = mul( ttrans( width/2, height/2 ), to_screen );
 
 		if( isButtonActive( draw_hats ) ) {
-			stream.push( `<use xlink:href="#${tiles[idx].svg_id}f" transform="matrix(${to_screen[0]} ${to_screen[3]} ${to_screen[1]} ${to_screen[4]} ${to_screen[2]+width/2} ${to_screen[5]+height/2})"/>` )
+			stream.push( getSVGInstance( tiles[idx].getSVGFillID(), S ) );
 		}
 		if( isButtonActive( draw_super ) ) {
-			stream.push( `<use xlink:href="#${tiles[idx].svg_id}s" transform="matrix(${to_screen[0]} ${to_screen[3]} ${to_screen[1]} ${to_screen[4]} ${to_screen[2]+width/2} ${to_screen[5]+height/2})"/>` )
+			stream.push( getSVGInstance( tiles[idx].getSVGStrokeID(), S ) );
 		}
 		stream.push( '</svg>' );
 
 		saveStrings( stream, 'output', 'svg' );
 	} );
-	box_height += 30;
+
+	addButton( "Save Matrices", function() {
+		const stream = [];
+		const idx = {'H':0, 'T':1, 'P':2, 'F':3}[radio.value()];
+		tiles[idx].getText( stream, ident );
+		saveStrings( stream, 'output', 'txt' );
+	} );
+
 	box_height -= 5; // remove half the padding
 }
 
